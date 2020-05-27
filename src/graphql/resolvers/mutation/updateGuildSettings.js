c4a151db8c98b5c2112a0933b4e751746e76c3b9
Jelle van Snik <jellevs@gmail.com>
2020-05-27 16:33:33 +0200

    Add prefix support

--- src/graphql/resolvers/mutation/updateGuildSettings.js
+++ src/graphql/resolvers/mutation/updateGuildSettings.js
@@ -2,30 +2,39 @@ const { makeGuildSettings } = require("../query/guildResolver");
 const { Long } = require("mongodb");
 
 const fields = {
-    mentionPrefix: "core",
-    deleteCommand: "core",
-    useEmbedForMessages: "core"
-}
+  mentionPrefix: { namespace: "core" },
+  deleteCommand: { namespace: "core" },
+  useEmbedForMessages: { namespace: "core" },
+  prefix: {
+    namespace: "core",
+    validate: (val) => {
+      if (val.length == 0) throw new Error("Prefix invalid");
+      return val; // TODO improve validation
+    },
+  },
+};
 
 function bodyBuilder(input) {
-    const body = {};
+  const body = {};
 
-    let i = 0;
-    for (let key in input) {
-      body[fields[key] + "." + key] = input[key]
-      i++;
-    }
-    if (i < 1)
-      return null;
-    return {
-        $set: body
-    }
+  let i = 0;
+  for (let key in input) {
+    if (fields[key] == null) continue;
+
+    let val = input[key];
+    if (fields[key].validate) val = fields[key].validate(val);
+    body[fields[key].namespace + "." + key] = val;
+    i++;
+  }
+  if (i < 1) return null;
+  return {
+    $set: body,
+  };
 }
 
 async function resolve(parent, { id, input }, { db }) {
   const updateBody = bodyBuilder(input);
-  if (!updateBody)
-    throw new Error("needupdatefield");
+  if (!updateBody) throw new Error("needupdatefield");
   const guild = await db.guilds.findOneAndUpdate(
     {
       _id: Long.fromString(id),
